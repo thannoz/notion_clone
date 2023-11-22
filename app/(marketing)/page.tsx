5cbd4636ac69545de6b63bdd9427d6b7780aa60e
thannoz <carlos-7@hotmail.de>
2023-11-22 23:31:08 +0100

    Started implementing the landing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
+import { Heading } from "./_components/heading";
 
 const Marketing = () => {
-  return <h2 className="text-red-500">Hello from Notion</h2>;
+  return (
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
+      </div>
+    </div>
+  );
 };
 
 export default Marketing;
