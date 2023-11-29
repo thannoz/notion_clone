45f906af9a30ddbfd736eca1e23ab7fac664a53d
thannoz <carlos-7@hotmail.de>
2023-11-29 17:41:09 +0100

    Setted up convex and clerk

---
+++ convex/auth.config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  providers: [
+    {
+      domain: "https://bursting-mule-79.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
