39819355156973d07862091302ef0d2f57cc59a2
thannoz <carlos-7@hotmail.de>
2023-12-03 20:13:22 +0100

    Started implementing the sidebar

---
+++ app/(main)/_components/navigation.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+export const Navigation = () => {
+  return (
+    <>
+      <aside
+        className="group/sidebar h-full bg-secondary overflow-y-auto 
+                relative flex w-60 flex-col z-[99999]"
+      >
+        <div>
+          <p>Action items</p>
+        </div>
+        <div className="mt-4">
+          <p>Documents</p>
+        </div>
+        <div className="opacity-0 group-hover/sidebar:opacity-100" />
+      </aside>
+    </>
+  );
+};
