92c38514dacee7e8e2aa51ca55141afed206a0b5
thannoz <carlos-7@hotmail.de>
2023-12-12 20:09:06 +0100

    Started implementing functionality for quering documents

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -2,11 +2,30 @@
 
 import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/clerk-react";
+import { useMutation } from "convex/react";
 import { PlusCircle } from "lucide-react";
 import Image from "next/image";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 const Documents = () => {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({ title: "Untitled" });
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created successfully",
+      error: "Failed to create a new note",
+    });
+  };
+
+  // const onFetch = () => {
+  //   const promise =
+  // }
+
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
       <Image
@@ -26,7 +45,7 @@ const Documents = () => {
       <h2 className="text-lg font-medium">
         Welcome to {user?.firstName}&apos;s Jotion
       </h2>
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircle className="h-4 w-4 mr-2" />
         Create a note
       </Button>
