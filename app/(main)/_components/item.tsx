5400ddadf59e2c27d0864274bdcd57fb1a69098e
thannoz <carlos-7@hotmail.de>
2023-12-13 23:28:34 +0100

    Continued working on the item component

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,25 +1,69 @@
 "use client";
 
-import { LucideIcon } from "lucide-react";
 import { FC } from "react";
+import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
+import { Id } from "@/convex/_generated/dataModel";
+import { cn } from "@/lib/utils";
 
 interface ItemProps {
   onClick: () => void;
   label: string;
   icon: LucideIcon;
+  id?: Id<"documents">;
+  documentIcon?: string;
+  active?: boolean;
+  isSearch?: boolean;
+  level?: number;
+  onExpand?: () => void;
+  expanded?: boolean;
 }
 
-export const Item: FC<ItemProps> = ({ label, onClick, icon: Icon }) => {
+export const Item: FC<ItemProps> = ({
+  label,
+  onClick,
+  icon: Icon,
+  id,
+  documentIcon,
+  active,
+  isSearch,
+  level = 0,
+  onExpand,
+  expanded,
+}) => {
+  const ChevronIcon = expanded ? ChevronDown : ChevronRight;
+
   return (
     <div
       onClick={onClick}
       role="button"
-      style={{ paddingLeft: "12px" }}
-      className="group min-h-[27px] text-sm py-1 pr-3 w-full 
-      hover:bg-primary/5 flex items-center text-muted-foreground font-medium"
+      style={{ paddingLeft: level ? `${level * 12 + 12}px` : "12px" }}
+      className={cn(
+        "group min-h-[27px] text-sm py-1 pr-3 w-full\
+      hover:bg-primary/5 flex items-center text-muted-foreground font-medium",
+        active && "bg-primary/5 text-primary"
+      )}
     >
-      <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+      {/* Check if id is provided i.e. user is logged in. If thats the case render a button */}
+      {!!id && (
+        <div
+          role="button"
+          className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
+          onClick={() => {}}
+        >
+          <ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground/50" />
+        </div>
+      )}
+      {documentIcon ? (
+        <div className="shrink-0 mr-2 text-[18px]">{documentIcon}</div>
+      ) : (
+        <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+      )}
       <span className="truncate">{label}</span>
+      {isSearch && (
+        <kbd>
+          <span></span>
+        </kbd>
+      )}
     </div>
   );
 };
