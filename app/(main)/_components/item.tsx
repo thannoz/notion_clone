48ef4ca8830c47a9fcf94c701c98b2ed19ea5fc5
thannoz <carlos-7@hotmail.de>
2023-12-21 23:27:19 +0100

    Finished functionality of expandind an item (document)

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
+import { ChevronDown, ChevronRight, LucideIcon, Plus } from "lucide-react";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
 import { Skeleton } from "@/components/ui/skeleton";
@@ -31,7 +31,8 @@ export const Item = ({
   expanded,
 }: ItemProps) => {
   const handleExpand = (evt: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    console.log("handleExpand");
+    evt.stopPropagation();
+    onExpand?.();
   };
 
   const ChevronIcon = expanded ? ChevronDown : ChevronRight;
@@ -52,7 +53,7 @@ export const Item = ({
         <div
           role="button"
           className="h-full rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 mr-1"
-          onClick={() => {}}
+          onClick={handleExpand}
         >
           <ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground/50" />
         </div>
@@ -64,9 +65,22 @@ export const Item = ({
       )}
       <span className="truncate">{label}</span>
       {isSearch && (
-        <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+        <kbd
+          className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 
+        rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100"
+        >
           <span className="text-xs">⌘</span>K
         </kbd>
+      )}
+      {!!id && (
+        <div className="ml-auto flex items-center gap-x-2">
+          <div
+            className="opacity-0 group-hover:opacity-100 h-full ml-auto
+          rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
+          >
+            <Plus className="h-4 w-4 text-muted-foreground" />
+          </div>
+        </div>
       )}
     </div>
   );
